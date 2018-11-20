0f23cba2148ef181a56ab4b3e3bf5018f70f77ba
Ammar The Trainer <ammarthetrainer@gmail.com>
2018-11-20 16:14:59 +0500

    Project Created

---
+++ Ch16-P2-LateBindingWithDynamic/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch16_P2_LateBindingWithDynamic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
